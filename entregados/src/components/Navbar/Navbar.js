5af595cf0ed02d6c977f9829fbca032f6912037b
unknown <cdor.olivafacundoj@gmail.com>
2022-08-18 22:48:01 -0300

    first commit

---
+++ entregados/src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import './Navbar.css';
+
+function Navbar (){
+    return(
+    <div className='alerta-contenedor'>
+        
+        <h1>HOCKEY SHOP</h1>
+        <p className='alerta-parrafo'>Tienda de hockey</p>
+        <nav>
+        <button>ARTICULOS</button>
+        <button>PROMOCIONES</button>
+        <button>PERSONALIZADOS</button>
+        <button>CONTACTO</button>
+        </nav>       
+    </div>
+    )
+}
+
+
+export default Navbar;
